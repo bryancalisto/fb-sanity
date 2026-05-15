be675c9df1f9a9139e3e191e267cb798514ca4ac
bryancalisto <bryancalisto@yahoo.es>
2026-05-15 10:41:20 -0500

    make handlers just funcs, not array of funcs

--- chrome/main.js
+++ chrome/main.js
@@ -12,91 +12,76 @@ function logError (...args) {
 
 const handlers = {
     'reactions-bar': {
-        hide: [
-            () => document.querySelectorAll('div[data-ad-rendering-role="like_button"]').forEach(item => {
-                const container = item.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+        hide: () => document.querySelectorAll('div[data-ad-rendering-role="like_button"]').forEach(item => {
+            const container = item.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
 
-                if (!container){
-                    logWarn('Could not find reactions bar container for item:', item);
-                    return;
-                }
+            if (!container){
+                logWarn('Could not find reactions bar container for item:', item);
+                return;
+            }
 
-                const firstChild = container.firstElementChild;
-                if (firstChild && firstChild.style.display !== 'none') {
-                    firstChild.style.display = 'none';
-                }
-            }),
-        ],
-        show: [
-            () => document.querySelectorAll('div[data-ad-rendering-role="like_button"]').forEach(item => {
-                const container = item.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+            const firstChild = container.firstElementChild;
+            if (firstChild && firstChild.style.display !== 'none') {
+                firstChild.style.display = 'none';
+            }
+        }),
+        show: () => document.querySelectorAll('div[data-ad-rendering-role="like_button"]').forEach(item => {
+            const container = item.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
 
-                if (!container){
-                    logWarn('Could not find reactions bar container for item:', item);
-                    return;
-                }
+            if (!container){
+                logWarn('Could not find reactions bar container for item:', item);
+                return;
+            }
 
-                const firstChild = container.firstElementChild;
-                if (firstChild && firstChild.style.display === 'none') {
-                    firstChild.style.display = '';
-                }
-            }),
-        ]
+            const firstChild = container.firstElementChild;
+            if (firstChild && firstChild.style.display === 'none') {
+                firstChild.style.display = '';
+            }
+        })
     },
     'full-container': {
-        hide: [
-            () => document.querySelectorAll('div[data-ad-rendering-role="like_button"]').forEach(item => {
-                const container = item.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+        hide: () => document.querySelectorAll('div[data-ad-rendering-role="like_button"]').forEach(item => {
+            const container = item.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
 
-                if (!container){
-                    logWarn('Could not find full container for item:', item);
-                    return;
-                }
+            if (!container){
+                logWarn('Could not find full container for item:', item);
+                return;
+            }
 
-                if (container.style.display !== 'none') {
-                    container.style.display = 'none';
-                }
-            })
-        ],
-        show: [
-            () => document.querySelectorAll('div[data-ad-rendering-role="like_button"]').forEach(item => {
-                const container = item.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
+            if (container.style.display !== 'none') {
+                container.style.display = 'none';
+            }
+        }),
+        show: () => document.querySelectorAll('div[data-ad-rendering-role="like_button"]').forEach(item => {
+            const container = item.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode;
 
-                if (!container){
-                    logWarn('Could not find full container for item:', item);
-                    return;
-                }
+            if (!container){
+                logWarn('Could not find full container for item:', item);
+                return;
+            }
 
-                if (container.style.display === 'none') {
-                    container.style.display = '';
-                }
-            })
-        ]
+            if (container.style.display === 'none') {
+                container.style.display = '';
+            }
+        })
     }
 }
 
 function applyReactionsBar(show) {
     const action = show ? 'show' : 'hide';
-    for (let i=0; i < handlers['reactions-bar'][action].length; i++) {
-        const handler = handlers['reactions-bar'][action][i];
-        try {
-            handler();
-        } catch(e){
-            logError(`Error applying reactions bar handler (${i}):`, e);
-        }
+    try {
+        handlers['reactions-bar'][action]();
+    } catch(e){
+        logError(`Error applying reactions bar handler:`, e);
     }
 }
 
 function applyFullContainer(show) {
     const action = show ? 'show' : 'hide';
-
-    for (let i=0; i < handlers['full-container'][action].length; i++) {
-        const handler = handlers['full-container'][action][i];
-        try {
-            handler();
-        } catch(e){
-            logError(`Error applying full container handler (${i}):`, e);
-        }
+    try {
+        handlers['full-container'][action]();
+    } catch(e){
+        logError(`Error applying full container handler:`, e);
     }
 }
 
